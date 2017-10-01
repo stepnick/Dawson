2f82cb13df80f0cbacd918701bbe10f46a548a46
stepnick <nikitka-stepanenko-2000@mail.ru>
2017-10-01 11:39:46 +0300

    uddated num1

--- chapter 5/Ch.5 num 1/Ch.5 num 1/Ch.5_num_1.py
+++ chapter 5/Ch.5 num 1/Ch.5 num 1/Ch.5_num_1.py
@@ -1,6 +1,29 @@
 import random 
-words = ["ОЧЕНЬ", "НАДОЕЛО", "УЧИТЬСЯ", "АНТИХАЙРП"]
+#import numpy 
+words = ["ОЧЕНЬ", "НАДОЕЛО", "УЧИТЬСЯ", "АНТИХАЙП"]
 str = []
-while len(words) > 0   :
-    str = words.pop(random.randint(0, len(words) - 1))
-    print(str)
+str1 = [1, 2, 3, 4 ]
+print(random.shuffle(words))
+numberDEL2 = len(words) // 2
+
+length = len(words)
+#while len(words) > 0   :
+    #str = words.pop(random.randint(0, len(words) - 1))
+    #print(str)
+
+for number in range(numberDEL2):
+    #x = random.choice(words)
+    #y = random.choice(words)
+    #z = x 
+    #x = y
+    #y = z 
+
+    x = random.randint(0, length)
+    y = random.randint(0, length)
+    buf = words[x]
+    words[x] = words[y]
+    words[y] = buf
+   
+
+for number in range(len(words)):
+    print(words[number])
